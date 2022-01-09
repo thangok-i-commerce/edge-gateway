1b96c69f4a16cc025b237ca8eadad17baf5e6fc5
thangnv2 <thangnv2@techcombank.com.vn>
2022-01-10 00:59:54 +0700

    add eureka

--- src/main/java/thangok/icommerce/edgegateway/EdgeGatewayApplication.java
+++ src/main/java/thangok/icommerce/edgegateway/EdgeGatewayApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteDefinition;
 import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 import java.util.ArrayList;
@@ -14,6 +15,7 @@ import java.util.List;
 
 @Slf4j
 @SpringBootApplication
+@EnableEurekaClient
 public class EdgeGatewayApplication {
 
     @Autowired
