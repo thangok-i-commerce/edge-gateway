e4eaa5137471002c94ccb2ac98a381cd551dfd79
thangnv2 <thangnv2@techcombank.com.vn>
2022-01-10 00:22:06 +0700

    config routing

--- src/main/java/thangok/icommerce/edgegateway/EdgeGatewayApplication.java
+++ src/main/java/thangok/icommerce/edgegateway/EdgeGatewayApplication.java
@@ -1,13 +1,42 @@
 package thangok.icommerce.edgegateway;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
+import org.springframework.context.annotation.Bean;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
 @SpringBootApplication
 public class EdgeGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EdgeGatewayApplication.class, args);
-	}
+    @Autowired
+    RouteDefinitionLocator locator;
+
+    @Bean
+    public List<GroupedOpenApi> apis() {
+        List<GroupedOpenApi> groups = new ArrayList<>();
+        List<RouteDefinition> definitions = locator.getRouteDefinitions().collectList().block();
+        definitions.forEach(x -> {
+            log.info(x.toString());
+        });
+        definitions.stream().filter(routeDefinition -> routeDefinition.getId().matches(".*Module")).forEach(routeDefinition -> {
+            String name = routeDefinition.getId().replaceAll("Module", "");
+            GroupedOpenApi.builder().pathsToMatch("/" + name + "/**").group(name).build();
+
+            log.info(name);
+        });
+        return groups;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdgeGatewayApplication.class, args);
+    }
 
 }
--- src/main/java/thangok/icommerce/edgegateway/security/ApplicationSecurity.java
+++ src/main/java/thangok/icommerce/edgegateway/security/ApplicationSecurity.java
@@ -1,13 +1,35 @@
 package thangok.icommerce.edgegateway.security;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
-@Configuration
-public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/**");
+
+@EnableWebFluxSecurity
+public class ApplicationSecurity {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http.authorizeExchange()
+                .pathMatchers("/**").permitAll()
+                .pathMatchers("/home/user").hasAnyRole("USER", "ADMIN")
+                .pathMatchers("/home/admin").hasRole("ADMIN")
+                .anyExchange()
+                .authenticated()
+                .and()
+                .formLogin();
+
+        http.csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
+
 }
