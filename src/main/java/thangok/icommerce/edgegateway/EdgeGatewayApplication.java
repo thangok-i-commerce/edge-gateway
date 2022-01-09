be8ce1a926abe5f3b9fa1e997eeca740e39accf7
thangnv2 <thangnv2@techcombank.com.vn>
2022-01-09 23:41:41 +0700

    Edge Gateway application

---
+++ src/main/java/thangok/icommerce/edgegateway/EdgeGatewayApplication.java
@@ -0,0 +1,13 @@
+package thangok.icommerce.edgegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdgeGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdgeGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/thangok/icommerce/edgegateway/EdgeGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package thangok.icommerce.edgegateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdgeGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
